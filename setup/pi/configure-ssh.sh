4d44d5b3a678ec9e1a1a54ef6abbd5b203a955f3
Thomas Rund <tom.rund@gmail.com>
2022-12-19 16:51:50 -0500

    Fix unbound variable error, remove unneccesary variables

--- setup/pi/configure-ssh.sh
+++ setup/pi/configure-ssh.sh
@@ -1,23 +1,17 @@
 #!/bin/bash -eu
 
 setup_progress "configuring ssh"
-auth_keys_file='/root/.ssh/authorized_keys'
-sshd_config_file='/etc/ssh/sshd_config'
 
-# If the user has defined a public key for root SSH access apply it
-if ! [ -z "${SSH_ROOT_PUBLIC_KEY}" ]
+# If requested, add the desired SSH public key into /root/.ssh/authorized_keys
+if [ "${SSH_ROOT_PUBLIC_KEY:-false}" != "false" ] && [ -n "${SSH_ROOT_PUBLIC_KEY}" ]
 then
-  # Don't overwrite the auth_keys_file if it already exists as the user may have modified it further
-  if ! [ -s "${auth_keys_file}" ]
-  then
-    echo "${SSH_ROOT_PUBLIC_KEY}" > "${auth_keys_file}"
-  fi
+  echo "${SSH_ROOT_PUBLIC_KEY}" > '/root/.ssh/authorized_keys'
 fi
 
-# Disable SSH Password Authentication if the user requested us to do so
+# If requested, disable SSH Password Authentication
 if [ "${SSH_DISABLE_PASSWORD_AUTHENTICATION:-false}" = "true" ]
 then
-  sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/' ${sshd_config_file}
+  sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/' '/etc/ssh/sshd_config'
   systemctl reload sshd
 fi
 
